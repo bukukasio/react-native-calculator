2119f0e4e46e62fb6ebf16ed0ab9a56bba8b0ea2
Gokul P <gokul@beecash.io>
2020-07-06 12:48:08 +0530

    changed icon style

--- src/Button.tsx
+++ src/Button.tsx
@@ -53,7 +53,7 @@ export class Button extends React.Component<ButtonProps> {
     }
 
     return (
-      <Image source={icon} style={{ width: 20, height: 20 }} />
+      <Image source={icon} />
     )
 
   }
